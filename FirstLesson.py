1d4383a850dc5e5434290f8f9d96b8fd5da1d18c
blabri10 <blabri.10@gmail.com>
2019-12-13 15:32:46 +0200

    wysiwyg

---
+++ FirstLesson.py
@@ -0,0 +1,8 @@
+# Working w strings & integers
+ourGod = "banana"
+eitansAge = 13
+Age = 14
+
+print("My god is:" + ourGod + " and my age is:" + str(Age))
+print("my age is {} eitans age is {}".format(Age, eitansAge))
+print(f' my age is {Age} eitans age is {eitansAge}')
